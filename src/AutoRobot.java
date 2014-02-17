390f46db7a631eaa95acbbd2cd76c191262fed53
nmbenz <nmbenz@olivet.edu>
2014-02-17 17:17:36 -0600

    Changed stack to type Direction Added new maze traversal/optimization implementation (optimization still broken, probably) Added third sensor support Deleted method 'update()' Added method 'scanTiles()' with 3 sensor support Updated DifferentialPilot calls to use move() method

--- src/AutoRobot.java
+++ src/AutoRobot.java
@@ -5,18 +5,16 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class AutoRobot {
 
     private Stack<Direction> path;
+      
+    private static final int TILE_SIZE = 115;
     
-    /*private static final int RIGHT = 1;
-    private static final int LEFT = 2;
-    private static final int CENTER = 0;*/
+    public static TileSet tiles;
     
-    private static final int TILE_SIZE = 115; //Measured in mm
-    public static final int CENTER = 0, RIGHT = 1, LEFT = 2;
-    public static Tile[] lights = new Tile[3];
     private static LightSensor lCenter = new LightSensor(SensorPort.S1);
     private static LightSensor lRight = new LightSensor(SensorPort.S2);
+    private static LightSensor lLeft = new LightSensor(SensorPort.S3);
+                                                                                                       
     private static DifferentialPilot pilot;
-    /*Tile possiblities*/
 
     public AutoRobot() {
         
@@ -26,10 +24,20 @@ public class AutoRobot {
         pilot.setTravelSpeed(100);
         pilot.setRotateSpeed(80);
     }
-    public void update(){
-     lights[CENTER] = Tile.parse(lCenter.readValue());
-     lights[RIGHT] = Tile.parse(lRight.readValue());
-     //lights[LEFT] =  Tile.parse(lLeft.readValue()));
+    
+    public static void main(String[] args){
+     AutoRobot bot = new AutoRobot();
+     bot.checkPath(Direction.FORWARD);
+    }
+    
+    public void scanTiles() {
+        Tile left, right, center;
+        
+        left = Tile.parse(lLeft.readValue());
+        center =  Tile.parse(lCenter.readValue());
+        right = Tile.parse(lRight.readValue());
+        
+        tiles = TileSet.getTileSet(left,right,center);
     }
 
 	public void move(Direction d){
@@ -37,9 +45,142 @@ public class AutoRobot {
 		pilot.travel(d.travel * TILE_SIZE);//linear component
 		path.push(d);//record movement
 	}
-    public static void main(String[] args){
-     AutoRobot bot = new AutoRobot();
-     bot.update();
-     bot.enterMaze();
+    
+    public boolean checkPath(Direction direction) {
+        int temp;
+        int count = 0; 		//Tracks # of moves into each path
+        boolean correctPath = true;
+        
+        while(true) {
+            try{Thread.sleep(300);}catch(Exception e){}
+            scanTiles();
+
+            if(tiles == TileSet.CENTER) {
+                pilot.travel(TILE_SIZE);
+                path.push(Direction.FORWARD);
+            }
+            
+            else if(tiles == TileSet.INTERSECTION) {
+                pilot.travel(TILE_SIZE/2);
+                path.push(Direction.FORWARD);
+                pilot.rotate(90);
+                path.push(Direction.SPIN_RIGHT);
+                checkPath(Direction.SPIN_RIGHT);
+                checkPath(Direction.FORWARD);
+                checkPath(Direction.SPIN_LEFT); 
+            }
+                    
+            else if(tiles == TileSet.RIGHT || tiles == TileSet.END_RIGHT) {
+                pilot.travel(TILE_SIZE/1.5);
+                path.push(Direction.FORWARD);
+                pilot.rotate(90);
+                path.push(Direction.SPIN_RIGHT);
+                checkPath(Direction.SPIN_RIGHT);
+                checkPath(Direction.FORWARD);
+            }
+        
+            else if(tiles == TileSet.LEFT || tiles == TileSet.END_LEFT) {
+                pilot.travel(TILE_SIZE/2);
+                path.push(Direction.FORWARD);
+                pilot.rotate(-90);
+                path.push(Direction.SPIN_LEFT);
+                checkPath(Direction.SPIN_LEFT);
+                checkPath(Direction.FORWARD);
+            }
+        
+            else if (tiles == TileSet.END) {
+                pilot.travel(TILE_SIZE);
+                path.push(Direction.FORWARD);
+                System.out.println("End found.");
+                while(true){}
+            }
+            
+            else if (tiles == TileSet.DEAD_END) {
+                reversePath(count);
+                switch(direction) {
+                    case SPIN_RIGHT:
+                        pilot.rotate(90);
+                        break;
+                    case SPIN_LEFT:
+                        pilot.rotate(-90);
+                        break;
+                    case FORWARD:
+                        pilot.rotate(180);
+                        break;        
+                }
+                correctPath = false;
+                break;
+            }
+        }
+        
+        return correctPath;
+    }   
+    
+    public void reversePath(int count) {
+        Direction d;
+        pilot.rotate(180);
+        
+        while(count > 0) {
+            d = path.pop();
+            switch(d) {
+                case SPIN_RIGHT:
+                    pilot.rotate(90);   
+                    break;
+                case SPIN_LEFT:
+                    pilot.rotate(-90);
+                    break;
+                case FORWARD:
+                    pilot.travel(TILE_SIZE);
+                    break;
+            }
+            count--;
+        }
+    }                            
+     
+    public void exitMaze() {
+        Direction d;
+        
+        path = invertTurns(path);
+        
+        while(!path.isEmpty()) {
+            d = path.pop();
+            switch(d) {
+                case SPIN_RIGHT:
+                    System.out.print('R');
+                    pilot.rotate(90);   
+                    break;
+                case SPIN_LEFT:
+                    System.out.print('L');
+                    pilot.rotate(-90);
+                    break;
+                case FORWARD:
+                    System.out.print('F');
+                    pilot.travel(TILE_SIZE);
+                    break;
+            }
+            try{Thread.sleep(400);} catch(Exception e) {}   
+        }
+    }
+    
+    public Stack<Direction> invertTurns(Stack<Direction> path) {
+        Stack<Direction> tempStack = new Stack<Direction>();
+        Direction temp;
+        
+        while(!path.isEmpty()){
+            tempStack.push(path.pop());
+        }
+        
+        while(!tempStack.isEmpty()) {
+            temp = tempStack.pop();
+            
+            if(temp == Direction.SPIN_RIGHT)           //This block inverts directions:
+                path.push(Direction.SPIN_LEFT);    // R=L, L=R, F=F
+            else if(temp == Direction.SPIN_LEFT)
+                path.push(Direction.SPIN_RIGHT);    
+            else
+                path.push(Direction.FORWARD);               
+        }
+        
+        return path;    
     }
 }
