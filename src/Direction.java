f3ff0834eb9b6aedc05f41b21ffb1b3b670f0ba9
whmeitzler <whmeitzler@olivet.edu>
2014-02-17 16:41:04 -0600

    Updated values to be accurate to the NXT

--- src/Direction.java
+++ src/Direction.java
@@ -1,21 +1,21 @@
 public enum Direction{
-	 STAY		( 0,   0, 0),
-     SPIN_BACK	( 0, 180, 0),
-     FORWARD	( 0,   0, 1),
-     BACKWARD	( 0,   0,-1),
-     ARC_LEFT	( 1,  90,-1),
-     ARC_RIGHT  (-1,  90,-1),
-     SPIN_LEFT	( 0,  90,-1),
-	 SPIN_RIGHT	( 0, -90,-1);
+	STAY		 ( 0,   0, 0),
+     SPIN_BACK	 ( 0, 180, 0),
+     FORWARD	 ( 0,   0, 1),
+     BACKWARD	 ( 0,   0,-1),
+     ARC_LEFT	 (-1,  -90, 0),
+     ARC_RIGHT  ( 1,  90, 0),
+     SPIN_LEFT	 ( 0,  90, 0),
+	SPIN_RIGHT ( 0, -90, 0);
 	 
     public int radius;
-	public int angle;
+    public int angle;
     public int travel;
 
     Direction(int radius, int angle, int travel){
         this.radius = radius;
-		this.angle = angle;
-	    this.travel = travel;
+	   this.angle = angle;
+	   this.travel = travel;
     }
     public Direction invert(){
      switch(this){
@@ -27,10 +27,10 @@ public enum Direction{
                return BACKWARD;
           case BACKWARD:
                return FORWARD;
-          case LEFT:
-               return RIGHT;  
-          case RIGHT:
-               return LEFT;
+          case SPIN_LEFT:
+               return SPIN_RIGHT;  
+          case SPIN_RIGHT:
+               return SPIN_LEFT;
 		  case ARC_LEFT:
 				return ARC_RIGHT;
 		  case ARC_RIGHT:
